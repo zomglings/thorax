3e4ec48785932949168daa46012908c657a5c28c
Neeraj Kashyap <nkashy1@gmail.com>
2021-04-30 13:11:06 -0700

    Initial working version of thorax

--- go.mod
+++ go.mod
@@ -3,10 +3,11 @@ module github.com/zomglings/thorax
 go 1.15
 
 require (
-	github.com/bugout-dev/bugout-go v0.3.2 // indirect
-	github.com/bugout-dev/humbug/go v0.0.0-20210427100933-329153577544 // indirect
-	github.com/google/uuid v1.2.0 // indirect
+	github.com/bmizerany/assert v0.0.0-20160611221934-b7ed37b82869 // indirect
+	github.com/bugout-dev/bugout-go v0.3.3
+	github.com/bugout-dev/humbug/go v0.0.0-20210427100933-329153577544
+	github.com/google/uuid v1.2.0
+	github.com/segmentio/analytics-go v3.1.0+incompatible // indirect
 	github.com/segmentio/backo-go v0.0.0-20200129164019-23eae7c10bd3 // indirect
 	github.com/xtgo/uuid v0.0.0-20140804021211-a0b114877d4c // indirect
-	gopkg.in/segmentio/analytics-go.v3 v3.1.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,10 +1,17 @@
-package thorax
+package main
 
 import (
+	"flag"
+	"fmt"
 	"os"
+	"strings"
+	"time"
 
+	bugout "github.com/bugout-dev/bugout-go/pkg"
+	spire "github.com/bugout-dev/bugout-go/pkg/spire"
 	humbug "github.com/bugout-dev/humbug/go/pkg"
 	"github.com/google/uuid"
+	"github.com/segmentio/analytics-go"
 )
 
 var thoraxReporterToken string = "357c7247-5f6e-4f16-83f1-6ae95dadc6ff"
@@ -29,4 +36,123 @@ func main() {
 
 	report := humbug.SystemReport()
 	reporter.Publish(report)
+
+	var segmentWriteKey, bugoutToken, bugoutJournalID, cursor string
+	var batchSize, timeout int
+	var debug bool
+	flag.StringVar(&segmentWriteKey, "segment", "", "Segment write key (get one by creating a source at https://segment.com)")
+	flag.StringVar(&bugoutToken, "token", "", "Bugout access token (create one at https://bugout.dev/account/tokens)")
+	flag.StringVar(&bugoutJournalID, "journal", "", "Bugout journal ID to load events from")
+	flag.StringVar(&cursor, "cursor", "", "(Optional) cursor from which to start loading items")
+	flag.IntVar(&batchSize, "N", 1000, "Number of reports to process per iteration")
+	flag.IntVar(&timeout, "s", 0, "Number of seconds to wait between Bugout requests")
+	flag.BoolVar(&debug, "debug", false, "Set this to true to run in debug mode")
+	flag.Parse()
+
+	if segmentWriteKey == "" {
+		panic("Please pass a segment write key using the -segment option.")
+	}
+
+	if bugoutToken == "" {
+		panic("Please pass a Bugout token using the -token option.")
+	}
+
+	if bugoutJournalID == "" {
+		panic("Please pass a Bugout journal ID using the -journal option.")
+	}
+
+	if debug {
+		fmt.Printf("DEBUG: Bugout access token: %s\n", bugoutToken)
+		fmt.Printf("DEBUG: Bugout journal ID: %s\n", bugoutJournalID)
+		fmt.Printf("DEBUG: Segment write key: %s\n", segmentWriteKey)
+		fmt.Printf("DEBUG: Cursor: %s\n", cursor)
+		fmt.Printf("DEBUG: Batch size: %d\n", batchSize)
+		fmt.Printf("DEBUG: Timeout: %d\n", timeout)
+	}
+
+	bugoutClient, err := bugout.ClientFromEnv()
+	if err != nil {
+		panic(err)
+	}
+
+	segmentClient, err := analytics.NewWithConfig(segmentWriteKey, analytics.Config{
+		Interval:  1 * time.Second,
+		BatchSize: 100,
+	})
+	if err != nil {
+		panic(err)
+	}
+	defer segmentClient.Close()
+
+	newCursor := cursor
+	halt := false
+	offset := 0
+	for {
+		results, err := reportsIterator(bugoutClient, bugoutToken, bugoutJournalID, cursor, batchSize, offset)
+		if err != nil {
+			panic(err)
+		}
+		loadingUntil := offset + batchSize
+		if results.TotalResults < loadingUntil {
+			loadingUntil = results.TotalResults
+		}
+		fmt.Printf("Loading to Segment: %d to %d of %d\n", offset, loadingUntil, results.TotalResults)
+		offset = results.NextOffset
+		if results.NextOffset == 0 {
+			halt = true
+		}
+		newCursor = loadToSegment(segmentClient, results.Results)
+		fmt.Printf("New cursor: %s\n", newCursor)
+		if halt {
+			break
+		} else {
+			time.Sleep(time.Duration(timeout) * time.Second)
+		}
+	}
+	fmt.Println("Done!")
+}
+
+func cleanTimestamp(rawTimestamp string) string {
+	return strings.ReplaceAll(rawTimestamp, " ", "T")
+}
+
+func reportsIterator(client bugout.BugoutClient, token, journalID, cursor string, limit, offset int) (spire.EntryResultsPage, error) {
+	var query string = ""
+	if cursor != "" {
+		cleanedCursor := cleanTimestamp(cursor)
+		query = fmt.Sprintf("created_at:>%s", cleanedCursor)
+	}
+	parameters := map[string]string{
+		"order":   "asc",
+		"content": "false",
+	}
+	return client.Spire.SearchEntries(token, journalID, query, limit, offset, parameters)
+}
+
+func loadToSegment(client analytics.Client, entries []spire.Entry) string {
+	for _, entry := range entries {
+		entryProperties := analytics.NewProperties()
+		sessionID := "unknown"
+		for _, tag := range entry.Tags {
+			components := strings.SplitN(tag, ":", 2)
+			if len(components) < 2 {
+				entryProperties.Set(tag, true)
+			} else {
+				entryProperties.Set(components[0], components[1])
+				if components[0] == "session" {
+					sessionID = components[1]
+				}
+			}
+		}
+
+		err := client.Enqueue(analytics.Track{
+			Event:      entry.Title,
+			UserId:     sessionID,
+			Properties: entryProperties,
+		})
+		if err != nil {
+			panic(err)
+		}
+	}
+	return cleanTimestamp(entries[len(entries)-1].CreatedAt)
 }
